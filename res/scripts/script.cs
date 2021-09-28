ea35a9e426958fdad88e282d02961d9b048fb460
Cvostr <cvosttv@gmail.com>
2021-09-28 23:58:35 +0300

    script

---
+++ res/scripts/script.cs
@@ -0,0 +1,37 @@
+using System;
+
+class script : EntityScript {
+    
+    private Vec3 test;
+
+    public void OnStart(){
+        test = new Vec3(1, 2, 3);
+        Logger.Log("test");
+        GetEntity().SetActive(false);
+        GetEntity().SetPosition(GetEntity().GetPosition() + test);
+        GetEntity().GetParent().SetName("parent");
+        Logger.Log(GetEntity().GetParent().GetName());
+
+        Entity child = GetEntity().GetScene().AddNewEntity("new entity from c#");
+        GetEntity().AddChild(child);
+
+        Logger.Log("printing children names");
+        Entity[] children = GetEntity().GetChildren();
+        for(uint i = 0; i < children.Length; i ++){
+            Logger.Log("Child " + children[i].GetName());
+        }
+    }
+
+    public void OnUpdate(){
+        if(Input.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)){
+            Logger.Log("Left pressed");
+        }
+        if(Input.IsKeyPressed(KeyCode.KEY_CODE_W)){
+            Logger.Log("W pressed");
+        }
+        if(Input.IsKeyHold(KeyCode.KEY_CODE_A)){
+            Logger.Log("A hold");
+        }
+        //Logger.Log("update");
+    }
+}
