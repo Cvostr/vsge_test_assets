b0c0657d6fb9ea981f6162515d37e04baeda8355
Cvostr <cvosttv@gmail.com>
2021-10-04 10:37:06 +0300

    update

---
+++ res/scripts/test_other_script.cs
@@ -0,0 +1,22 @@
+using System;
+
+class test_other_script : EntityScript {
+    
+    private int value;
+
+    public void OnStart(){
+       
+    }
+
+    public void OnUpdate(){
+    
+    }
+
+    public int GetValue(){
+        return value;
+    }
+
+    public void SetValue(int val){
+        value = val;
+    }
+}
